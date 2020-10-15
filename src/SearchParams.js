e1eb05b7afc54743cadf7e5559450ae1bcacb35d
Jonathan O <jonathan@ejdigitalmedia.com>
2020-10-14 23:53:35 -0400

    Added async call to Pet API to get pet info and images, and added data to Pet component

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,16 +1,47 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from "./Results";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]); // Empty array because there aren't any pets when you first request them from the API
+
+  // async functions always return a Promise when they complete
+  async function requestPets() {
+    // Wait in this function until pet.animals() finishes running and return that data
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal,
+    });
+
+    // You know animals will be set because of requestPets() above, but if nothing gets returned then set an empty array instead
+    setPets(animals || []);
+  }
+
+  useEffect(() => {
+    setBreeds([]); // Clear out existing list of breeds
+    setBreed(""); // Clear out previously selected breed
+
+    // Note: This breeds isn't the breeds above
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name); // Get string version of the current breed's name
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]); // List of dependencies that React will check for changes, and it'll re-run useEffect if any changed
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault(); // You don't want to actually submit the form through HTML POST or something like that
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -27,81 +58,9 @@ const SearchParams = () => {
         <BreedDropdown />
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
 
 export default SearchParams;
-
-// // OLD WAY, but it works
-// import React, { useState } from "react";
-// import { ANIMALS } from "@frontendmasters/pet";
-
-// const SearchParams = () => {
-//   const [location, setLocation] = useState("Seattle, WA");
-//   const [animal, setAnimal] = useState("dog");
-//   const [breed, setBreed] = useState("");
-//   const [breeds, setBreeds] = useState([]);
-
-//   return (
-//     <div className="search-params">
-//       <form>
-//         <label htmlFor="location">
-//           Location
-//           <input
-//             id="location"
-//             value={location}
-//             placeholder="Location"
-//             onChange={(event) => {
-//               setLocation(event.target.value);
-//             }}
-//           />
-//         </label>
-//         <label htmlFor="animal">
-//           Animal
-//           <select
-//             id="animal"
-//             value={animal}
-//             onChange={(event) => {
-//               setAnimal(event.target.animal);
-//             }}
-//             onBlur={(event) => {
-//               setAnimal(event.target.animal);
-//             }}
-//           >
-//             <option>All</option>
-//             {ANIMALS.map((animal) => (
-//               <option key={animal} value={animal}>
-//                 {animal}
-//               </option>
-//             ))}
-//           </select>
-//         </label>
-//         <label htmlFor="breed">
-//           Breed
-//           <select
-//             id="breed"
-//             value={breed}
-//             onChange={(event) => {
-//               setBreed(event.target.breed);
-//             }}
-//             onBlur={(event) => {
-//               setBreed(event.target.breed);
-//             }}
-//             disabled={breeds.length === 0}
-//           >
-//             <option>All</option>
-//             {breeds.map((breedString) => (
-//               <option key={breedString} value={breedString}>
-//                 {breedString}
-//               </option>
-//             ))}
-//           </select>
-//         </label>
-//         <button>Submit</button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default SearchParams;
