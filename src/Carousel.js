adf37c17ad9dde57b2c76042005e4cdfad0fa8aa
Jonathan O <jonathan@ejdigitalmedia.com>
2020-10-18 21:05:59 -0400

    Updated Details to include animal information and added Carousel for multiple images

---
+++ src/Carousel.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      photos: [],
+      active: 0,
+    };
+
+    this.handleIndexClick = this.handleIndexClick.bind(this);
+  }
+
+  static getDerivedStateFromProps({ media }) {
+    // Default placeholder image
+    let photos = ["http://placecorgi.com/600/600"];
+
+    // If media contains at least 1 photo, replace the placeholder with that data
+    if (media.length) {
+      photos = media.map(({ large }) => large);
+    }
+
+    return { photos };
+  }
+
+  handleIndexClick(event) {
+    this.setState({
+      active: +event.target.dataset.index,
+    });
+  }
+
+  render() {
+    const { photos, active } = this.state;
+
+    return (
+      <div className="carousel">
+        <img src={photos[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => (
+            // eslint-disable-next-line
+            <img
+              key={photo}
+              onClick={this.handleIndexClick}
+              data-index={index}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
