c78647a20a07c875a8479b579b1b4a866a1d7f0d
Jonathan O <jonathan@ejdigitalmedia.com>
2020-11-01 16:17:44 -0500

    Added jest and tests using React Testing Library

---
+++ __mocks__/@frontendmasters/pet.js
@@ -0,0 +1,34 @@
+import { readFileSync } from "fs";
+import path from "path";
+import { act } from "@testing-library/react";
+
+const breeds = [
+  { name: "Bichon Frise" },
+  { name: "Bolognese" },
+  { name: "Coton de Tulear" },
+  { name: "Havanese" },
+  { name: "Maltese" },
+];
+
+const dogs = JSON.parse(
+  readFileSync(path.join(__dirname, "res.json")).toString()
+);
+
+export const ANIMALS = ["dog", "cat", "bird"];
+export const _breeds = breeds;
+export const _dogs = dogs.animals;
+
+const mock = {
+  breeds: jest.fn(() => {
+    return {
+      then: (callback) => act(() => callback({ breeds })),
+    };
+  }),
+  animals: jest.fn(() => {
+    return {
+      then: (callback) => act(() => callback(dogs)),
+    };
+  }),
+};
+
+export default mock;
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -12,17 +12,16 @@ const SearchParams = () => {
   const [pets, setPets] = useState([]); // Empty array because there aren't any pets when you first request them from the API
   const [theme, setTheme] = useContext(ThemeContext); // You aren't going to need setTheme so grabbing it isn't necessary
 
-  // async functions always return a Promise when they complete
-  async function requestPets() {
-    // Wait in this function until pet.animals() finishes running and return that data
-    const { animals } = await pet.animals({
-      location,
-      breed,
-      type: animal,
-    });
-
-    // You know animals will be set because of requestPets() above, but if nothing gets returned then set an empty array instead
-    setPets(animals || []);
+  function requestPets() {
+    pet
+      .animals({
+        location,
+        breed,
+        type: animal,
+      })
+      .then(({ animals }) => {
+        setPets(animals || []);
+      });
   }
 
   useEffect(() => {
